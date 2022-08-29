da06189f675ee377d4718c1d489b1dabebe5171b
Osakpolor Obaseki <obasekiosa@gmail.com>
2022-08-29 17:29:09 +0100

    Implement Key generation service

--- src/main/java/com/example/pagaassgn/kgs/KGS.java
+++ src/main/java/com/example/pagaassgn/kgs/KGS.java
@@ -1,21 +1,65 @@
 package com.example.pagaassgn.kgs;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
 public class KGS {
     private KeyRepository keyRepository;
 
+//    @Value("${kgs.key_length")
+    private Integer KEY_LENGTH = 6;
+
     @Autowired
-    public KGS(KeyRepository repository) {
-        this.keyRepository = repository;
+    public KGS(KeyRepository keyRepository) {
+        this.keyRepository = keyRepository;
     }
 
     public Key getUnusedKey() {
-        throw new UnsupportedOperationException("This method has not been implemented");
+
+        return null;
     }
 
-    public Key generateNewKey() {
-        throw new UnsupportedOperationException("This method has not been implemented");
+    private String byteToHex(byte[] bytes) {
+        StringBuilder sb = new StringBuilder();
+        for (byte b : bytes) {
+            sb.append(String.format("%X", b));
+
+            if (sb.length() >= this.KEY_LENGTH) {
+                break;
+            }
+        }
+
+        return sb.substring(0, this.KEY_LENGTH).toString();
+    }
+
+    public Key generateNewKey() throws KeyCollisionException {
+        String uuid = null;
+        try {
+            MessageDigest salt = null;
+            try {
+                salt = MessageDigest.getInstance("SHA-256");
+            } catch (NoSuchAlgorithmException e) {
+                throw new RuntimeException(e);
+            }
+
+            salt.update(UUID.randomUUID().toString().getBytes("UTF-8"));
+
+            uuid = byteToHex(salt.digest());
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
+        }
+        // check for collision here //
+
+        Key key = new Key(uuid, false);
+        return this.keyRepository.save(key);
     }
 
     public boolean deleteKey(Key key) {
@@ -26,7 +70,28 @@ public class KGS {
         throw new UnsupportedOperationException("This method has not been implemented");
     }
 
-    public boolean activateKey(Key key) {
-        throw new UnsupportedOperationException("This method has not been implemented");
+    public boolean activateKey(String key) {
+        Optional<Key> keyEntry = keyRepository.findById(key);
+        if (keyEntry.isPresent() ) {
+            Key keyValue = keyEntry.get();
+            if (!keyValue.isActive()) {
+                keyValue.setActive(true);
+                keyRepository.save(keyValue);
+            }
+            return true;
+        }
+
+        return false;
     }
+
+    public Key getKey(String key) {
+        Optional<Key> keyEntry = keyRepository.findById(key);
+        if (keyEntry.isPresent()) {
+            return  keyEntry.get();
+        }
+
+        return null;
+    }
+
+
 }
