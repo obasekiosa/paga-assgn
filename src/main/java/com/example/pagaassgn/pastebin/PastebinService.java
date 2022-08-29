0e6a987e6e7cc927ef9860c14ce839907a4e5906
Osakpolor Obaseki <obasekiosa@gmail.com>
2022-08-29 07:33:02 +0100

    Update pastebin service

--- src/main/java/com/example/pagaassgn/pastebin/PastebinService.java
+++ src/main/java/com/example/pagaassgn/pastebin/PastebinService.java
@@ -1,4 +1,21 @@
 package com.example.pagaassgn.pastebin;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class PastebinService {
+
+    private UserRepository userRepository;
+    private PastebinRepository pastebinRepository;
+
+    @Autowired
+    public PastebinService(UserRepository userRepository, PastebinRepository pastebinRepository) {
+
+        this.userRepository = userRepository;
+        this.pastebinRepository = pastebinRepository;
+    }
+
+
+
+
+
 }
