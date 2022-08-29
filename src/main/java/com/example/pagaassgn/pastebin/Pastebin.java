cbf162cb40400b23a8914829e9f88c4601fd6c1a
Osakpolor Obaseki <obasekiosa@gmail.com>
2022-08-29 07:19:25 +0100

    Update pastebin model

--- src/main/java/com/example/pagaassgn/pastebin/Pastebin.java
+++ src/main/java/com/example/pagaassgn/pastebin/Pastebin.java
@@ -1,4 +1,14 @@
 package com.example.pagaassgn.pastebin;
 
+import java.util.Date;
+
 public class Pastebin {
+    private String id;
+    private long createdBy;
+    private Date expiry;
+    private Date createdOn;
+
+    private String content;
+
+    public Pastebin() {}
 }
