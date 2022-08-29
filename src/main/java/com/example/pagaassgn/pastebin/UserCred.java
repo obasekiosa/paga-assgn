ae13ef62bbf4a7fd7ab6a7432dd4b6a10d5fc97c
Osakpolor Obaseki <obasekiosa@gmail.com>
2022-08-29 07:19:08 +0100

    Add usercred model

---
+++ src/main/java/com/example/pagaassgn/pastebin/UserCred.java
@@ -0,0 +1,12 @@
+package com.example.pagaassgn.pastebin;
+
+import java.util.Date;
+
+public class UserCred {
+    private long id;
+    private long userId;
+    private Date createdOn;
+    private Date updatedOn;
+    private String password;
+    private String salt;
+}
