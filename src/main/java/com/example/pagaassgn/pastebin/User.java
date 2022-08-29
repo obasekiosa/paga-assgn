42427b8aa7905230fdbccccfe9dbd62f869a30fa
Osakpolor Obaseki <obasekiosa@gmail.com>
2022-08-29 07:47:41 +0100

    Implement user constructor

--- src/main/java/com/example/pagaassgn/pastebin/User.java
+++ src/main/java/com/example/pagaassgn/pastebin/User.java
@@ -5,5 +5,13 @@ import java.util.Date;
 public class User {
     private long id;
     private String username;
+
     private Date createdOn;
+
+    public User() {
+    }
+
+    public User(String username) {
+        this.username = username;
+    }
 }
