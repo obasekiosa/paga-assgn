e1c55d41ab8d3f0b61a2f487915123d2ee59e3b5
Osakpolor Obaseki <obasekiosa@gmail.com>
2022-08-29 21:26:54 +0100

    Implement Scheduler

--- src/main/java/com/example/pagaassgn/Scheduler.java
+++ src/main/java/com/example/pagaassgn/Scheduler.java
@@ -1,16 +1,31 @@
 package com.example.pagaassgn;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Scheduler {
 
-    private int MAX_AVAILABLE_KEYS;
+//    @Value("#{app.config.max_inactive_keys:#{100}")
+    @Value("#{new Integer('${app.config.max_inactive_keys:100}')}")
+    private Integer MAX_AVAILABLE_KEYS;
+
+    @Autowired
+    private AppServices appServices;
 
     // run every 12 hrs
+    @Scheduled(cron = "${app.schedule.cron.generate_keys}")
     public void generateKeys() {
-        throw new UnsupportedOperationException("This method has not been implemented");
+        appServices.generateKeys(MAX_AVAILABLE_KEYS);
     }
 
     // run every minute
+    @Scheduled(cron = "${app.schedule.cron.expired_bins}")
     public void removedExpiredBins() {
-        throw new UnsupportedOperationException("This method has not been implemented");
+        appServices.removeExpiredBins();
     }
 }
