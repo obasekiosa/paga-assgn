d34f4151a29af81abbbb4337e915ce7042d7d6f8
Osakpolor Obaseki <obasekiosa@gmail.com>
2022-08-29 17:28:25 +0100

    Add JPA annotations for Key models

--- src/main/java/com/example/pagaassgn/kgs/Key.java
+++ src/main/java/com/example/pagaassgn/kgs/Key.java
@@ -1,13 +1,57 @@
 package com.example.pagaassgn.kgs;
 
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.util.Date;
 
+@Entity
+@Table(name = "keys")
 public class Key {
 
-    private String key;
+    @Id
+    private String id;
     private Date createdOn;
     private boolean active;
 
     public Key() {}
+
+    public Key(String key, boolean active) {
+        this.id = key;
+        this.active = active;
+    }
+
+    public String getKey() {
+        return id;
+    }
+
+    public void setKey(String key) {
+        this.id = key;
+    }
+
+    public Date getCreatedOn() {
+        return createdOn;
+    }
+
+    public void setCreatedOn(Date createdOn) {
+        this.createdOn = createdOn;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    @Override
+    public String toString() {
+        return "Key{" +
+                "id='" + id + '\'' +
+                ", createdOn=" + createdOn +
+                ", active=" + active +
+                '}';
+    }
 }
--- src/main/java/com/example/pagaassgn/kgs/KeyRepository.java
+++ src/main/java/com/example/pagaassgn/kgs/KeyRepository.java
@@ -1,6 +1,9 @@
 package com.example.pagaassgn.kgs;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
-public interface KeyRepository extends JpaRepository<String, Key> {
+@Repository
+public interface KeyRepository extends CrudRepository<Key, String> {
 }
