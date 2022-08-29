989947062f733068fe3dd2b5f4aa1ee790d74b58
Osakpolor Obaseki <obasekiosa@gmail.com>
2022-08-29 19:18:12 +0100

    Add removal of expired bins as an app service

--- src/main/java/com/example/pagaassgn/AppServices.java
+++ src/main/java/com/example/pagaassgn/AppServices.java
@@ -2,14 +2,22 @@ package com.example.pagaassgn;
 
 import com.example.pagaassgn.kgs.KGS;
 import com.example.pagaassgn.kgs.KeyCollisionException;
+import com.example.pagaassgn.pastebin.PastebinService;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.List;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class AppServices {
 
     @Autowired
     private static KGS kgs;
+
+    @Autowired
+    private static PastebinService pastebinService;
+
+    private static final Logger LOGGER = Logger.getLogger(AppServices.class.getName());
 
     public static int generateKeys(int numOfKeys) {
         if (numOfKeys <= 0 ) return  0;
@@ -26,5 +34,15 @@ public class AppServices {
         }
 
         return numOfKeys - collisionCount;
+    }
+
+    public static int removeExpiredBins() {
+        List<String> keys = pastebinService.deleteExpiredPasteBins();
+        int reactivatedCount = kgs.batchActivateKeys(keys);
+        if (keys.size() != reactivatedCount) {
+            LOGGER.log(Level.INFO, "Number of Re-Activated keys (" + keys.size() + ")" +
+                    " is not the same as Number of deleted keys (" + reactivatedCount + ")");
+        }
+        return keys.size();
     }
 }
