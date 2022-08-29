2bf86533acdc624af2dd30de81bcbbb73445b9ad
Osakpolor Obaseki <obasekiosa@gmail.com>
2022-08-29 06:37:53 +0100

    initial commit

---
+++ src/main/java/com/example/pagaassgn/PagaAssgnApplication.java
@@ -0,0 +1,13 @@
+package com.example.pagaassgn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PagaAssgnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PagaAssgnApplication.class, args);
+    }
+
+}
