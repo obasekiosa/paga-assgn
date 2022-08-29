d9db0e4173fbb6163faa862f314264b3b7d30e6f
Osakpolor Obaseki <obasekiosa@gmail.com>
2022-08-29 08:00:16 +0100

    Add scheduled processes

--- src/main/java/com/example/pagaassgn/Scheduler.java
+++ src/main/java/com/example/pagaassgn/Scheduler.java
@@ -1,4 +1,16 @@
 package com.example.pagaassgn;
 
 public class Scheduler {
+
+    private int MAX_AVAILABLE_KEYS;
+
+    // run every 12 hrs
+    public void generateKeys() {
+        throw new UnsupportedOperationException("This method has not been implemented");
+    }
+
+    // run every minute
+    public void removedExpiredBins() {
+        throw new UnsupportedOperationException("This method has not been implemented");
+    }
 }
