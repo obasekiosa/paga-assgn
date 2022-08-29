7540cc32c6468ac12df39741296c0454ef5bb688
Osakpolor Obaseki <obasekiosa@gmail.com>
2022-08-29 17:29:45 +0100

    Implement Pastebin service

--- src/main/java/com/example/pagaassgn/pastebin/PastebinService.java
+++ src/main/java/com/example/pagaassgn/pastebin/PastebinService.java
@@ -1,17 +1,66 @@
 package com.example.pagaassgn.pastebin;
 
+import com.example.pagaassgn.auth.UserRepository;
+import com.example.pagaassgn.auth.UserService;
+import com.example.pagaassgn.kgs.KGS;
+import com.example.pagaassgn.kgs.Key;
+import com.example.pagaassgn.kgs.KeyCollisionException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.Calendar;
+import java.util.Optional;
+
+@Service
 public class PastebinService {
 
-    private UserRepository userRepository;
+    private UserService userService;
     private PastebinRepository pastebinRepository;
 
     @Autowired
-    public PastebinService(UserRepository userRepository, PastebinRepository pastebinRepository) {
+    private KGS kgs;
 
-        this.userRepository = userRepository;
+    @Autowired
+    public PastebinService(UserService userService, PastebinRepository pastebinRepository) {
+
+        this.userService = userService;
         this.pastebinRepository = pastebinRepository;
+
+    }
+
+    public String getAnonymousBin(String id) {
+        Optional<Pastebin> binEntry = this.pastebinRepository.findById(id);
+        return binEntry.map(Pastebin::getContent).orElse(null);
+    }
+
+
+    public String createAnonymousBin(String content, Integer duration) {
+        Key key = kgs.getUnusedKey();
+        String id;
+        if (key != null) {
+            id = key.getKey();
+        } else {
+            try {
+                key = kgs.generateNewKey();
+                id = key.getKey();
+            } catch (KeyCollisionException e) {
+                System.out.println("Key collision occurred."); //Todo: switch to system logger
+                return null;
+            }
+        }
+
+        Pastebin bin = pastebinRepository.save(new Pastebin(id, 0, content));
+        kgs.activateKey(id);
+
+        if (duration != null) {
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTime(bin.getCreatedOn());
+            calendar.add(Calendar.HOUR_OF_DAY, duration.intValue());
+
+            bin.setExpiry(calendar.getTime()); // Todo: have the database manage this calculation
+            bin = pastebinRepository.save(bin);
+        }
+        return bin.getId();
     }
 
 
